624120e79ac00bf69056aa35689959069ec9cbb1
susfinx <113091653+susfinx@users.noreply.github.com>
2023-07-24 14:14:07 +0300

    Update DZ-4-1.py

--- DZ-4-1.py
+++ DZ-4-1.py
@@ -1,1 +1,16 @@
+def transpose_matrix(matrix):
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
 
+    transposed_matrix = [[0 for _ in range(num_rows)] for _ in range(num_cols)]
+
+    for i in range(num_rows):
+        for j in range(num_cols):
+            transposed_matrix[j][i] = matrix[i][j]
+
+    return transposed_matrix
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+transposed = transpose_matrix(matrix)
+print(transposed)
