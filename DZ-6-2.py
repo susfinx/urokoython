bffc729c0c5d85d8e41bae6ec53f0774986a5a04
susfinx <113091653+susfinx@users.noreply.github.com>
2023-08-05 01:40:11 +0300

    Create DZ-6-2.py

---
+++ DZ-6-2.py
@@ -0,0 +1,20 @@
+__all__ = ['has_attacking_queens']
+
+import random
+
+def is_attacking(q1, q2):
+    return q1[0] == q2[0] or q1[1] == q2[1] or abs(q1[0] - q2[0]) == abs(q1[1] - q2[1])
+
+def has_attacking_queens(queens):
+    for i in range(8):
+        for j in range(i + 1, 8):
+            if is_attacking(queens[i], queens[j]):
+                return True
+    return False
+
+def generate_random_queens():
+    queens = [(random.randint(1, 8), random.randint(1, 8)) for _ in range(8)]
+    return queens
+
+if __name__ == "__main__":
+    print (has_attacking_queens(generate_random_queens()))
