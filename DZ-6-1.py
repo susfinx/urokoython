083c73a9408fcb264d29f49cff8e29e0b65d964e
susfinx <113091653+susfinx@users.noreply.github.com>
2023-08-05 01:06:27 +0300

    Update DZ-6-1.py

--- DZ-6-1.py
+++ DZ-6-1.py
@@ -1,1 +1,22 @@
 
+import sys
+from datetime import datetime
+
+def check_date(date_str):
+    try:
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+        return True, date.strftime('%A')
+    except ValueError:
+        return False, None
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python date_checker.py YYYY-MM-DD")
+    else:
+        input_date = sys.argv[1]
+        is_valid, day_of_week = check_date(input_date)
+        
+        if is_valid:
+            print(f"The date {input_date} is valid and falls on a {day_of_week}.")
+        else:
+            print(f"The date {input_date} is not valid.")
